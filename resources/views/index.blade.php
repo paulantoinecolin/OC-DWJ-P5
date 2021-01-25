b9a82a74f8c9c137abd8f1d712ee82e8a135961f
Paul <45489053+paulantoinecolin@users.noreply.github.com>
2021-01-25 23:07:38 +0100

    front content started

---
+++ resources/views/index.blade.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="bg-gray-100 px-10">
+    <header>
+        <nav class="menu flex justify-between items-stretch">
+            <div class="px-8">
+                <img class="h-40" src="/images/logo-efa.jpg" alt="logo-efa">
+            </div>
+            <ol class="flex justify-between items-stretch py-20">
+                <li class="active item"><a href="#">Accueil</a></li>
+                <li class="item"><a href="#">Clubs enfants</a></li>
+                <li class="item">Evenements</li>
+                <li class="item">Contact</li>
+            </ol>
+            <img class="h-10" src="/images/logo-facebook.jpg" alt="facebook-icon">
+            <img class="h-10" src="/images/logo-lang.png" alt="switch-lang">
+        </nav>
+    </header>
+
+    <section>
+        <h1 class="py-10 text-4xl font-bold text-center">Bilinguisme ne veut pas dire élitisme</h1>
+        <q>Tout sujet peut accéder au bilinguisme s’il bénéficie d’une immersion en langue régionale et/ou étrangère dès le plus jeune âge.
+            Pr Jean Petit, Psycholinguiste</q>
+
+        <p>Comment garantir une immersion parfaite en allemand à nos enfants tout en vivant en France ?
+            Pour maintenir un bilinguisme équilibré, EFA a créé trois ateliers ludiques germanophones où nos petits jouent, chantent et s’amusent exclusivement dans la langue de Goethe et la perfectionnent « en parlant ».  
+            Grâce à la variété des contacts sociaux, ils enrichissent leur allemand dans tous les domaines. Cela leur permet également de faire connaissance des coutumes et de la culture de leur parent allemand.</p>
+    </section>
+
+    <div id="kids-clubs"></div>
+        <h2 class="py-10 text-2xl font-bold text-center">Clubs pour enfants</h2>
+        <section class="flex justify-between items-stretch">
+            <div class="card">
+                <img src="" alt="">
+                <h5>0 à 3 ans</h5>
+                <p>En allemand, nos petits vont jouer, chanter, raconter des histoires, rire et faire des petits jeux de motricité. Bien qu’ils n’aillent pas encore à l’école, l’entourage français les conditionne à parler français. Ce groupe offre une parfaite occasion de stimuler l’allemand de nos bambins. Simultanément, les parents se souviennent ou apprennent des chants d’enfance. Les échanges en allemand entre grands et petits ont déjà montré ses fruits.</p>
+                <button>Plus d'informations</button>
+            </div>
+            <div class="card">
+                <img src="" alt="">
+                <h5>3 à 6 ans</h5>
+                <p>Souvent la matinée se déroule autour d'un thème : parfois tout le monde se transforme en animaux sauvages, où bien nous préparons une salade de fruits délicieuse avec des fruits apportés. Sont proposés des chansons, des jeux de doigts et et des jeux de motricité ; des travaux artistiques ne doivent pas manquer car les petits artistes adorent rapporter "une oeuvre d'art" à la maison. Tous les enfants ont déjà entendu l'allemand dans leur familles, certains ne sont pas encore habitués à parler en allemand. Des rituels tellesque la chanson du début et de la fin aident à faire des premiers pas, d'autres jeux sollicitent les enfants à devenir plus actifs. Küken- et Tigerentenclub, c'est vivre un moment en allemand et s'amuser en même temps. Atelier animé par une éducatrice allemande.</p>
+                <button>Plus d'informations</button>
+            </div>
+            <div class="card">
+                <img src="" alt="">
+                <h5>7 à 10 ans</h5>
+                <p>Avec la participation des enfants, nous organisons des activités sportives et créatives pour jouer et communiquer en allemand.</p>
+                <button>Plus d'informations</button>
+            </div>
+        </section>
+
+    <div id="family">
+        <h2 class="py-10 text-2xl  font-bold text-center">Activités en Famille</h2>
+            <section class="flex">
+                <img class="rounded-lg shadow" src="/images/photo1.png" alt="">
+                <p>CarnavalDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli
+                    Brunch de PâquesDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli
+                    Fête d’étéDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli</p>
+            </section>
+            <section class="flex py-10">
+                <p>Fabrication de lanternesDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli
+                    Défilé aux lanternes pour les enfants - MartinsumzugDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli
+                    Fabrication de décorations de NoëlDescription de l’évenementJe souhaite venir -> Lien vers formulaire pré-rempli</p>
+                <img class="rounded-lg shadow" src="/images/photo2.png" alt="">
+            </section>
+    </div>
+    
+    <section>
+        <h2>Nous Contacter</h2>
+        <form action=""></form>
+        <div id="map"></div>
+    </section>
+
+    <footer class="py-10">
+        <address>Présidente : Stephanie Zillmer
+            07 51 89 89 89
+            contact@efa-34.org</address>
+        <p>© 2020 EFA – Eveil Franco Allemand</p>
+    </footer>
+</body>
+</html>
